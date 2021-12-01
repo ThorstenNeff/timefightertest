a23f77ace31b3be534fb867e4801e031e9b519d2
jannikpaul <jannik_paul@gmx.de>
2021-12-01 20:44:17 +0100

    Eingabefeld hinzugefügt

--- app/src/main/java/de/jannikpaul/timefighter/EnterNameActivity.kt
+++ app/src/main/java/de/jannikpaul/timefighter/EnterNameActivity.kt
@@ -1,6 +1,11 @@
 package de.jannikpaul.timefighter
 
 import android.os.Bundle
+import android.util.Log
+import android.view.KeyEvent
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class EnterNameActivity : AppCompatActivity() {
@@ -8,6 +13,19 @@ class EnterNameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_enter_name)
+
+        val eingabeFeld = findViewById<EditText>(R.id.editTextTextPersonName)
+        eingabeFeld.setOnKeyListener(object : View.OnKeyListener {
+            override fun onKey(view: View?, keyCode: Int, keyEvent: KeyEvent?): Boolean {
+                if (keyEvent?.action == KeyEvent.ACTION_DOWN &&
+                    keyCode == KeyEvent.KEYCODE_ENTER) {
+                    Log.d("Eingabe", "Enter")
+                    return true
+                }
+                return false
+            }
+
+        })
 
     }
 }
